85626b6626d45835a942261e56e816e490dbf6cf
Alejandro Toledo <alex@Alejandros-MacBook-Pro.local>
2018-04-30 14:29:49 -0500

    Half of Part 1

--- part1/blockchain.js
+++ part1/blockchain.js
@@ -1,10 +1,42 @@
 "use strict";
 
 var SHA256 = require("crypto-js/sha256");
+var Crypto = require("crypto-js");
 
-// Object.assign(module.exports,{
-//    TODO
-// });
+
+
+
+ Object.assign(module.exports,{
+    blocks: [
+        {
+            data: "genesis!",
+            hash: "000000",
+            index: 0,
+            prevHash: undefined,
+            timestamp: 1523291999654,
+        }],
+    addBlock: function(text){
+        let block = {
+            data: text,
+            hash: "000001",
+            index: this.blocks[this.blocks.length - 1].index + 1,
+            prevHash: this.blocks[this.blocks.length-1].hash,
+            timestamp: Date.now(),
+        }
+        this.blocks.push(block);
+    },
+    createBlockHash: function(data){
+        return Crypto.SHA256(`${data.prevHash};${data.index};${data.data};${data.timestamp}`).toString();
+    },
+    print: function(){
+
+    },
+    isValid: function(){
+        
+    }
+ });
+
+
 
 
 // Hints:
